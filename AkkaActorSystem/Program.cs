ffc1a190d33e4983df0a2839be0b20122c0f5542
profesor79 <git@profesor79.pl>
2019-02-12 22:18:58 +0000

    executed script

---
+++ AkkaActorSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AkkaActorSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
