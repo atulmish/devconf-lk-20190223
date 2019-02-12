6ca52aa7aadc7bbbddb35fc116c8423f2d798a14
profesor79 <git@profesor79.pl>
2019-02-12 23:03:21 +0000

    task02 ready for extended props

--- AkkaActorSystem.Tests/Task01/AliceActorTests.cs
+++ AkkaActorSystem.Tests/Task01/AliceActorTests.cs
@@ -1,7 +1,31 @@
+using System;
+using Akka.Actor;
+using AkkaActorSystem.Task01;
+using Xunit;
+
 namespace AkkaActorSystem.Tests.Task01
 {
     public class AliceActorTests:ActorTestBase
     {
+        [Fact]
+        public void WhenStartsThenSendMessage()
+        {
+            var props = Props.Create(() => new AliceActor(_testProbe));
+            _sut = Sys.ActorOf(props);
+            _testProbe.ExpectMsg<Messages.AuthRequest>();
+        }
+
+
+        [Fact]
+        public void WhenReceivesAuthResponseThenSendNextRequest()
+        {
+            var props = Props.Create(() => new AliceActor(_testProbe));
+            _sut = Sys.ActorOf(props);
+
+            _sut.Tell(new Messages.AuthResponse());
+            ExpectMsg<Messages.AnotherAuthRequest>();
+        }
+
 
     }
 }
--- AkkaActorSystem/Task01/AliceActor.cs
+++ AkkaActorSystem/Task01/AliceActor.cs
@@ -4,9 +4,21 @@ namespace AkkaActorSystem.Task01
 {
     public class AliceActor:ReceiveActor
     {
-        public AliceActor()
-        {
+        private readonly IActorRef _bob;
 
+        public AliceActor(IActorRef bob)
+        {
+            _bob = bob;
+            Receive<Messages.AuthResponse>(m =>
+            {
+                Sender.Tell(new Messages.AnotherAuthRequest());
+            });
+        }
+
+        protected override void PreStart()
+        {
+            _bob.Tell(new Messages.AuthRequest());
+            base.PreStart();
         }
     }
 }
