3912592e04fc7f9eb05791334dbd432c91f1efd5
profesor79 <git@profesor79.pl>
2019-02-15 23:28:35 +0000

    task02 ready - timer message

--- AkkaActorSystem/Task02/AliceTimerActor.cs
+++ AkkaActorSystem/Task02/AliceTimerActor.cs
@@ -1,12 +1,47 @@
+using System;
 using Akka.Actor;
+using Akka.Event;
 
 namespace AkkaActorSystem.Task02
 {
     public class AliceTimerActor:ReceiveActor
     {
-        public AliceTimerActor()
-        {
+        private int _sum;
+        private ICancelable _cancelTimer;
 
+        public AliceTimerActor(IActorRef destination)
+        {
+            var logger = Context.GetLogger();
+            Receive<TimerMessages.AddValue>(a =>
+            {
+                logger.Info($"adding value: {a.Amount}");
+                _sum += a.Amount;
+                logger.Info($"Sum after operation: {_sum}");
+            });
+
+            Receive<TimerMessages.TimerTick>(timer =>
+            {
+                logger.Info($"Timer message received {DateTime.UtcNow}");
+                destination.Tell(new TimerMessages.SummarisedValue(_sum));
+                logger.Info($"Message sent to: {destination.Path}");
+            });
         }
+
+        protected override void PreStart()
+        {
+            _cancelTimer = new Cancelable(Context.System.Scheduler);
+
+            Context.System.Scheduler.ScheduleTellRepeatedly(
+                TimeSpan.FromSeconds(5),
+                TimeSpan.FromSeconds(5),
+                Self,
+                new TimerMessages.TimerTick(),
+                Self,
+                _cancelTimer);
+
+            base.PreStart();
+        }
+
+
     }
 }
