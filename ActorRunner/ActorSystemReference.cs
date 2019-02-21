fe39d2891deb0078258a1df11dca2591b2d82374
profesor79 <git@profesor79.pl>
2019-02-21 22:16:16 +0000

    added runner project

---
+++ ActorRunner/ActorSystemReference.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Diagnostics;
+using System.Net;
+using Akka.Actor;
+using Akka.Configuration;
+using Serilog;
+using Serilog.Events;
+
+namespace ActorRunner
+{
+    public static class ActorSystemReference
+    {
+        public static ActorSystem ActorsSystem;
+
+        private static Config GetConfig()
+        {
+            var hostname = Dns.GetHostName();
+            Console.WriteLine(hostname);
+            var config = ConfigurationFactory.ParseString(
+                hocon: @"
+                    akka {
+loglevel=INFO,  loggers=[""Akka.Logger.Serilog.SerilogLogger, Akka.Logger.Serilog""]
+                        actor {
+                            provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                        }
+                        remote {
+                            dot-netty.tcp {
+                                port = 9000
+                                hostname = ""greglaptop""
+                                public-hostname = ""greglaptop""
+
+                                  # Sets the send buffer size of the Sockets,
+                                  # set to 0b for platform default
+                                  send-buffer-size = 33554432b
+
+                                  # Sets the receive buffer size of the Sockets,
+                                  # set to 0b for platform default
+                                  receive-buffer-size = 33554432b
+
+                                  # Maximum message size the transport will accept, but at least
+                                  # 32000 bytes.
+                                  # Please note that UDP does not support arbitrary large datagrams,
+                                  # so this setting has to be chosen carefully when using UDP.
+                                  # Both send-buffer-size and receive-buffer-size settings has to
+                                  # be adjusted to be able to buffer messages of maximum size.
+                                  maximum-frame-size = 16777216b
+
+
+                            }
+                        }
+                    }
+                    ");
+
+            return config;
+        }
+
+        /// <summary>The main.</summary>
+        /// <param name="args">The args.</param>
+        public static void StartSystem()
+        {
+            Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console()
+                        .CreateLogger();
+
+
+            var assembly = global::System.Reflection.Assembly.GetExecutingAssembly();
+            var fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            var version = fvi.FileVersion;
+            Log.Information($"App version: {version}");
+
+
+            var config = GetConfig();
+            ActorsSystem = ActorSystem.Create("actorSystem", config);
+
+        }
+    }
+}
--- ActorRunner/Program.cs
+++ ActorRunner/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Akka.Actor;
+using AkkaActorSystem.Task06;
+using Serilog;
 
 namespace ActorRunner
 {
@@ -6,7 +9,37 @@ namespace ActorRunner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("provide option:");
+            var input = Console.ReadLine();
+            var doWeHaveInt = int.TryParse(input, out int value);
+            if (doWeHaveInt)
+            {
+                Console.WriteLine(value);
+                ActorSystemReference.StartSystem();
+                if (value == 1)
+                {
+                    Console.WriteLine("starting recovery example");
+                    StartRecoveryExample();
+
+                }
+
+                ActorSystemReference.ActorsSystem.WhenTerminated.Wait();
+            }
+
+      }
+
+        private static void StartRecoveryExample()
+        {
+            Console.WriteLine("?????");
+            Log.Information("creating actor");
+            var props = Props.Create(() => new ExampleAtLeastOnceDeliveryReceiveActor());
+            var actor = ActorSystemReference.ActorsSystem.ActorOf(props);
+            var msgCount = 0;
+            for (int i = 0; i < msgCount; i++)
+            {
+                Console.WriteLine(i);
+                actor.Tell("Do something");
+            }
         }
     }
 }
